a9498dcaaa27f82ed2f8fb9ff97256ba86c488de
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-26 21:15:37 -0700

    make test for isSunk() from Ship class
    test for two diff sizes and checks if isSunk() returns the
    correct response with the corresponding amount of hits

--- battleShipClasses.test.js
+++ battleShipClasses.test.js
@@ -2,15 +2,82 @@ import Ship from './src/Ship';
 import GameBoard from './src/GameBoard';
 import Player from './src/Player';
 
+describe('test for classes to be defined', () => {
+  test('Ship defined', () => {
+    expect(Ship).toBeDefined();
+  });
+  test('GameBoard defined', () => {
+    expect(GameBoard).toBeDefined();
+  });
+  test('Player defined', () => {
+    expect(Player).toBeDefined();
+  });
+});
 
-describe('test for classes to be defined', () =>{
-    test('Ship defined', () =>{
-        expect(Ship).toBeDefined();
-    });
-    test('GameBoard defined', () =>{
-        expect(GameBoard).toBeDefined();
-    });
-    test('Player defined', () =>{
-        expect(Player).toBeDefined();
-    });
+describe('test for Ship isSunk() size 4', () => {
+  let currShip;
+
+  beforeEach(() => {
+    currShip = new Ship(4);
+  });
+
+  test('check isSunk() with 1 hit', () => {
+    currShip.hit();
+    expect(currShip.isSunk()).toBe(false);
+  });
+
+  test('check isSunk() 4 with hits', () => {
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    expect(currShip.isSunk()).toBe(true);
+  });
+
+  test('check isSunk() 2 with hits', () => {
+    currShip.hit();
+    currShip.hit();
+    expect(currShip.isSunk()).toBe(false);
+  });
+});
+
+describe('test for Ship isSunk() size 10', () => {
+  let currShip;
+
+  beforeEach(() => {
+    currShip = new Ship(10);
+  });
+
+  test('check isSunk() with 1 hit', () => {
+    currShip.hit();
+    expect(currShip.isSunk()).toBe(false);
+  });
+
+  test('check isSunk() 4 with hits', () => {
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    expect(currShip.isSunk()).toBe(false);
+  });
+
+  test('check isSunk() 2 with hits', () => {
+    currShip.hit();
+    currShip.hit();
+    expect(currShip.isSunk()).toBe(false);
+  });
+
+  test('check isSunk() 2 with hits', () => {
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    currShip.hit();
+    expect(currShip.isSunk()).toBe(true);
+  });
 });
