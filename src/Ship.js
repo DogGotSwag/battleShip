595eeda55b3223010b8aa38c2a091f257b1b633b
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-26 21:17:11 -0700

    create logic for Ship aka isSunk() isHit()
    isSunk will check if a ship is sunk meaning it has taken as many hits
    as its length. isHit will add a hit to ship when called
    
    both work as expected and pass both tests

--- src/Ship.js
+++ src/Ship.js
@@ -1,3 +1,18 @@
-class Ship {}
+class Ship {
+  #hits = 0;
+
+  constructor(length) {
+    this.shipLength = length;
+  }
+
+  hit() {
+    this.#hits += 1;
+  }
+
+  isSunk() {
+    if (this.shipLength - this.#hits <= 0) return true;
+    return false;
+  }
+}
 
 export default Ship;
