920e7887c9a599530d2cb687389c081dcc78d267
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-28 12:11:09 -0700

    make GameBoard's receiveAttack() and this.#grid
    function will take a coordinate and respond with either a hit or a miss
    of a ship. This will be crucial when playing battleship so user knowns
    the outcome of there choice. this commits is including private property
    grid which holds all the positions and has the ships inside
    this version passes the current tests

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,3 +1,34 @@
-class GameBoard {}
+import Ship from './Ship';
+
+class GameBoard {
+
+  #grid = [
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+    ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+  ];
+
+  setShipAt(arr) {
+  const currShip = new Ship(4);
+    this.#grid[arr[0]][arr[1]] = currShip;
+  }
+
+  receiveAttack(arr) {
+    const coordinate = this.#grid[arr[0]][arr[1]];
+    if (typeof coordinate === 'object') {
+      coordinate.hit();      
+      return 'Hit';
+    }
+    return 'Miss';
+  }
+}
 
 export default GameBoard;
