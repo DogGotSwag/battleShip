5b271604cae43f11dad9557369625405076cb799
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-29 12:40:06 -0700

    add allSunk() to GameBoard
    function will be used to check if all the ships inside current GameBoard
    have been sunk.
    code now passes the tests created, i created a new private property
    called allShips to store all the made ships inside of it this useful
    as it allows to me to check all the ships in one go quickly

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,8 +1,9 @@
 import Ship from './Ship';
 
 class GameBoard {
-
   #missedShots = [];
+
+  #allShips = [];
 
   #grid = [
     ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
@@ -18,17 +19,18 @@ class GameBoard {
     ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
   ];
 
-  getGrid(){
+  getGrid() {
     return this.#grid;
   }
 
-  getMissedShots(){
+  getMissedShots() {
     return this.#missedShots;
   }
 
   setShipAt(...arr) {
     const currShip = new Ship(arr.length);
-    for(let i = 0; i < arr.length ; i += 1){
+    this.#allShips.push(currShip);
+    for (let i = 0; i < arr.length; i += 1) {
       const currCoordinate = arr[i];
       this.#grid[currCoordinate[0]][currCoordinate[1]] = currShip;
     }
@@ -37,11 +39,22 @@ class GameBoard {
   receiveAttack(arr) {
     const coordinate = this.#grid[arr[0]][arr[1]];
     if (typeof coordinate === 'object') {
-      coordinate.hit();      
+      coordinate.hit();
       return 'Hit';
     }
-    this.#missedShots.push([arr[0], arr[1]]);    
+    this.#missedShots.push([arr[0], arr[1]]);
     return 'Miss';
+  }
+
+  allSunk() {
+    const ships = this.#allShips;
+    for (let i = 0; i < ships.length; i += 1) {
+      const currShip = ships[i];
+      if (currShip.isSunk() === false) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
