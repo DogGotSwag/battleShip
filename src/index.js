b6a0b2ab2f339622e0f62809e6fab20245a6d959
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-31 13:13:52 -0700

    set a ship for real and computer player
    this ships will be used to make sure the I can display
    the ships correctly for each player

--- src/index.js
+++ src/index.js
@@ -6,3 +6,6 @@ const indexPlayer = new Player();
 
 const realPlayerBoard = indexPlayer.realPlayer.gameBoard;
 const computerPlayerBoard = indexPlayer.computerPlayer.gameBoard;
+
+realPlayerBoard.setShipAt([0,1], [0,2], [0,3]);
+computerPlayerBoard.setShipAt([6,3], [7,3], [8,3], [9,3]);
