3802f3f1bef386f67c15116b17c5548d750c32c0
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-31 13:10:27 -0700

    make Player instance and player board variables
    player board variables hold the grids of each players
    respective board making it easy to use its methods

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './style.css';
-import Player from'./Player';
+import Player from './Player';
 import {} from './domChanger';
+
+const indexPlayer = new Player();
+
+const realPlayerBoard = indexPlayer.realPlayer.gameBoard;
+const computerPlayerBoard = indexPlayer.computerPlayer.gameBoard;
