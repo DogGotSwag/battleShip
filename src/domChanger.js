5e7c7fc5b8af0dcf06b352720127fc76972fdcd0
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-12-31 18:15:19 -0700

    make renderPlayersBoards() populate board rows
    rows are to make the coordinates much easier to organize and style

--- src/domChanger.js
+++ src/domChanger.js
@@ -1,3 +1,13 @@
-function renderPlayersBoards() {}
+function renderPlayersBoards() {
+    const boards = document.querySelectorAll('.player');
+    
+    for(let i = 0; i < 2; i += 1){
+        const current = boards[i];
+        for(let j = 0; j < 10; j += 1){
+            const row = document.createElement('div');
+            current.appendChild(row);
+        }
+    }
+}
 
 export { renderPlayersBoards };
