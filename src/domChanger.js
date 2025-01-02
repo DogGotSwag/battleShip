91b759656a7c2f00338c7ce24c73ff57fe5fae20
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-02 12:02:22 -0700

    prettify code
    makes it look nice and readable

--- src/domChanger.js
+++ src/domChanger.js
@@ -1,22 +1,22 @@
 import './domChangerStyles.css';
 
 function renderPlayersBoards() {
-    const boards = document.querySelectorAll('.player');
-    
-    for(let i = 0; i < 2; i += 1){
-        const current = boards[i];
-        for(let j = 0; j < 10; j += 1){
-            const row = document.createElement('div');
-            row.className = 'boardRow';
-            current.appendChild(row);
+  const boards = document.querySelectorAll('.player');
 
-            for(let k = 0; k < 10; k += 1){
-                const coordinateBox = document.createElement('div');
-                coordinateBox.className = 'coordinate';
-                row.appendChild(coordinateBox);
-            }
-        }
+  for (let i = 0; i < 2; i += 1) {
+    const current = boards[i];
+    for (let j = 0; j < 10; j += 1) {
+      const row = document.createElement('div');
+      row.className = 'boardRow';
+      current.appendChild(row);
+
+      for (let k = 0; k < 10; k += 1) {
+        const coordinateBox = document.createElement('div');
+        coordinateBox.className = 'coordinate';
+        row.appendChild(coordinateBox);
+      }
     }
+  }
 }
 
 export { renderPlayersBoards };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './style.css';
 import Player from './Player';
-import {renderPlayersBoards} from './domChanger';
+import { renderPlayersBoards } from './domChanger';
 
 const indexPlayer = new Player();
 
 const realPlayerBoard = indexPlayer.realPlayer.gameBoard;
 const computerPlayerBoard = indexPlayer.computerPlayer.gameBoard;
 
-realPlayerBoard.setShipAt([0,1], [0,2], [0,3]);
-computerPlayerBoard.setShipAt([6,3], [7,3], [8,3], [9,3]);
+realPlayerBoard.setShipAt([0, 1], [0, 2], [0, 3]);
+computerPlayerBoard.setShipAt([6, 3], [7, 3], [8, 3], [9, 3]);
 
 renderPlayersBoards();
