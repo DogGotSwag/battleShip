a080578b98987ece932e0d84a1366b5c9f2a0572
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-06 14:08:45 -0700

    make module return a random coordinate
    now computer can pick random coordinates to attack player
    board

--- src/computerAttack.js
+++ src/computerAttack.js
@@ -1,5 +1,14 @@
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
+function generateRandomCoordinate(){
+    const newCoordinate = [];
+    newCoordinate[0] = getRandomNumber(0,10);
+    newCoordinate[1] = getRandomNumber(0,10);
+    return newCoordinate;
+}
 
 export default () =>{
-    
+    return generateRandomCoordinate();
 }
