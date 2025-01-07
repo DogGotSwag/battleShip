7d7e734a1e0fe3a7b5fa553910754bb59a6caeb1
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-06 19:13:53 -0700

    make computerAttack return random coordinate

--- src/computerAttack.js
+++ src/computerAttack.js
@@ -1,14 +1,12 @@
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-function generateRandomCoordinate(){
-    const newCoordinate = [];
-    newCoordinate[0] = getRandomNumber(0,10);
-    newCoordinate[1] = getRandomNumber(0,10);
-    return newCoordinate;
+function generateRandomCoordinate() {
+  const newCoordinate = [];
+  newCoordinate[0] = getRandomNumber(0, 10);
+  newCoordinate[1] = getRandomNumber(0, 10);
+  return newCoordinate;
 }
 
-export default () =>{
-    return generateRandomCoordinate();
-}
+export default () => generateRandomCoordinate()
