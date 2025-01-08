d0e051ca4e05c96a62d6361762ca3feec8c07da2
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-07 20:38:15 -0700

    refactor computerAttack to use availableShots()
    previous approach worked but but became more compute heavy
    as the number of coordinates available when down, new approach
    has O(1) complexity

--- src/computerAttack.js
+++ src/computerAttack.js
@@ -2,11 +2,7 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function generateRandomCoordinate() {
-  const newCoordinate = [];
-  newCoordinate[0] = getRandomNumber(0, 10);
-  newCoordinate[1] = getRandomNumber(0, 10);
-  return newCoordinate;
+export default (availableCoordinates) =>{
+  const index = getRandomNumber(0,availableCoordinates.length);
+  return availableCoordinates[index];
 }
-
-export default () => generateRandomCoordinate()
