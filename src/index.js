c4e35ca6ff4b3c3965248cea75ae8d2bc86d5f3f
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-09 13:19:55 -0700

    make first if statement check type & parent
    this is to make the code look more simple and understandable
    also gets rid of unneeded syntax

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import Player from './Player';
-import { renderPlayersBoards, renderBoard , gameOver} from './domChanger';
+import { renderPlayersBoards, renderBoard, gameOver } from './domChanger';
 import computerAttack from './computerAttack';
 
 const indexPlayer = new Player();
@@ -19,41 +19,38 @@ boards.forEach((board) => {
   board.addEventListener('click', (e) => {
     const clicked = e.target;
     const clickedType = clicked.classList[0];
-    if (clickedType === 'coordinate') {
-      const parent = clicked.parentNode.parentNode.classList[0];
+    const parent = clicked.parentNode.parentNode.classList[0];
+    if (clickedType === 'coordinate' && parent === 'computerPlayer') {
       const clickedPosition = clicked.classList[1].slice(1, 3).split('');
+      const hitOrMiss = computerPlayerBoard.receiveAttack(clickedPosition);
+      renderBoard(
+        'computerPlayer',
+        computerPlayerBoard.getGrid(),
+        computerPlayerBoard.getMissedShots(),
+        computerPlayerBoard.getHitShots()
+      );
+      if (computerPlayerBoard.allSunk() === true) {
+        gameOver('realPlayer', 'computerPlayer');
+      }
 
-      if (parent === 'computerPlayer') {
-        const hitOrMiss = computerPlayerBoard.receiveAttack(clickedPosition);
-        renderBoard(
-          'computerPlayer',
-          computerPlayerBoard.getGrid(),
-          computerPlayerBoard.getMissedShots(),
-          computerPlayerBoard.getHitShots()
-        );
-        if (computerPlayerBoard.allSunk() === true){
-          gameOver('realPlayer','computerPlayer');
-        }
+      if (hitOrMiss === 'Miss') {
+        let continueAttack;
+        do {
+          continueAttack = realPlayerBoard.receiveAttack(
+            computerAttack(realPlayerBoard.availableCoordinates())
+          );
+          renderBoard(
+            'realPlayer',
+            realPlayerBoard.getGrid(),
+            realPlayerBoard.getMissedShots(),
+            realPlayerBoard.getHitShots()
+          );
 
-        if (hitOrMiss === 'Miss') {
-          let continueAttack;
-          do {
-            continueAttack = realPlayerBoard.receiveAttack(
-              computerAttack(realPlayerBoard.availableCoordinates())
-            );
-            renderBoard(
-              'realPlayer',
-              realPlayerBoard.getGrid(),
-              realPlayerBoard.getMissedShots(),
-              realPlayerBoard.getHitShots()
-            );
-
-            if (realPlayerBoard.allSunk() === true){
-              gameOver('computerPlayer','realPlayer');
-              continueAttack = '';
-            }
-          } while (continueAttack === 'Hit');
-        }
+          if (realPlayerBoard.allSunk() === true) {
+            gameOver('computerPlayer', 'realPlayer');
+            continueAttack = '';
+          }
+        } while (continueAttack === 'Hit');
       }
     }
   });
