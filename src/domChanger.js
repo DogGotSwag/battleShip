e920707126faac3591b1f984fd3192e6905f0b3b
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-10 13:51:02 -0700

    Allow user to input there own coordinates
    move player vs computer logic to its own module
    
    this makes index file easier to manage and allows me to later makes
    a two player option much easier later on
    
    make makeCoordinateForm() with parameter
    
    function is already set to export and will be used to make a form
    to get the coordinates for each ship
    
    make module call makeCoordinateForm() first
    
    now i need to get the coordinates first then make the boards
    so for now I'll only make it get the coordinates
    
    replace double quotes with singles
    
    add parameter to makeCoordinateForm()
    
    will be used to make sure the form gets put on the
    player board
    
    add form to player board
    
    will be used to get coordinates for the ships
    
    make form take up all the board
    
    create layout & feildset/legend to form
    
    set correct white color on the form
    
    for border and text the color was wrong
    
    make button area & add css for layout
    
    will be used to store buttons, css to make it match the
    already existing layout
    
    add inputs form elements into the form
    
    10 for every single one of the ships that need to
    be set
    
    add css for form layout and text color
    
    add css styling to form and input elements
    
    makes it look nicer and input default stylings are very ugly
    
    add for/id attributes to form elements
    
    now the labels now link to there corresponding inputs
    
    put correct text on the input labels
    
    now each label has the correct text saying the size of the ship
    they use
    
    create reset/submit buttons
    
    style form buttons and set default css
    
    makes the buttons looks nicer and default css makes styling easier
    
    add text to buttons and style
    
    set resetButton to reset type
    
    so it can clear the form when clicked
    
    make inputs required
    
    makes sure all inputs are being used
    
    prevent default submit action of the form
    
    this so the form does not reset the page and i can use the info
    on the inputs to set ships
    
    fix: forgot to add class for last input
    
    get all input values when form is submitted
    
    i now i can extract the coordinates from the values
    
    get all coordinates inside of an array
    
    now i can have all the coordinates very neatly organized
    inside array so i can set ships
    
    move all get coordinates logic to its own function
    
    makes the code more understandable and easier to apply changes to
    logic
    
    adjust playerVsComputer() to array of coordinates
    
    it now uses the array of coordinates to set the ships for the player
    
    call playerVsComputer() with coordinates
    
    user can now set there own coordinates

--- src/domChanger.js
+++ src/domChanger.js
@@ -2,11 +2,10 @@ import './domChangerStyles.css';
 
 function renderPlayersBoards(players = 'player') {
   const boards = document.querySelectorAll(`.${players}`);
-  
-  for(let i = 0; i < boards.length; i += 1){
+
+  for (let i = 0; i < boards.length; i += 1) {
     boards[i].innerHTML = '';
   }
- 
 
   for (let i = 0; i < boards.length; i += 1) {
     const current = boards[i];
@@ -29,46 +28,100 @@ function renderPlayersBoards(players = 'player') {
   }
 }
 
-
-function renderBoard(player, grid, missedShots, hitShots){
+function renderBoard(player, grid, missedShots, hitShots) {
   renderPlayersBoards(player);
 
-  for(let i = 0; i < missedShots.length; i += 1){
+  for (let i = 0; i < missedShots.length; i += 1) {
     const shot = missedShots[i];
     const box = document.querySelector(`.${player} .k${shot[0]}${shot[1]}`);
     box.classList.add('missed');
   }
 
-  for(let i = 0; i < hitShots.length; i += 1){
+  for (let i = 0; i < hitShots.length; i += 1) {
     const shot = hitShots[i];
     const ship = grid[shot[0]][shot[1]];
-    
+
     const box = document.querySelector(`.${player} .k${shot[0]}${shot[1]}`);
 
-    if(ship.isSunk()) box.classList.add('sunk');
+    if (ship.isSunk()) box.classList.add('sunk');
 
     box.classList.add('hitShot');
   }
 }
 
-function disablePlay(){
+function disablePlay() {
   const playerBoard = document.querySelector('.computerPlayer');
   playerBoard.classList.add('disabled');
 }
 
-function displayWinner(player){
+function displayWinner(player) {
   const winner = document.querySelector(`.${player}`);
   winner.classList.add('winner');
 }
 
-function displayLoser(player){
+function displayLoser(player) {
   const loser = document.querySelector(`.${player}`);
   loser.classList.add('loser');
 }
 
-function gameOver(winner, loser){
+function gameOver(winner, loser) {
   displayWinner(winner);
   displayLoser(loser);
   disablePlay();
 }
-export { renderPlayersBoards, renderBoard, gameOver };
+
+function makeCoordinateForm(player) {
+  const container = document.querySelector(`.${player}`);
+  const frag = document.createDocumentFragment();
+
+  const form = document.createElement('form');
+  form.classList.add('getCoordinateForm');
+  frag.appendChild(form);
+
+  const fieldSet = document.createElement('fieldset');
+  fieldSet.classList.add('formFieldSet');
+
+  const legend = document.createElement('legend');
+  legend.innerText = 'Set Coordinates';
+  form.appendChild(fieldSet);
+  fieldSet.appendChild(legend);
+
+  const forAttrArray =  ['four', 'three', 'threeTwo', 'two', 'twoTwo', 'twoThree', 'one', 'oneTwo', 'oneThree', 'oneFour'];
+  const labelTexts = [4,3,3,2,2,2,1,1,1,1];
+
+  const ul = document.createElement('ul');
+  for (let i = 0; i < forAttrArray.length; i += 1) {
+    const li = document.createElement('li');
+    const label = document.createElement('label');
+    label.setAttribute('for', forAttrArray[i]);
+    label.innerText = `Ship Size ${labelTexts[i]} :`;
+    const input = document.createElement('input');
+    input.required = true;
+    input.setAttribute('id', forAttrArray[i]);
+
+    li.appendChild(label);
+    li.appendChild(input);
+    ul.appendChild(li);
+  }
+  fieldSet.appendChild(ul);
+
+  const buttonArea = document.createElement('div');
+  buttonArea.classList.add('buttonArea');
+
+  const resetButton = document.createElement('button');
+  resetButton.innerText = 'Reset';
+  resetButton.setAttribute('type','reset');
+
+  const submitButton = document.createElement('button');
+  submitButton.innerText = 'Submit';
+
+  buttonArea.appendChild(resetButton);
+  buttonArea.appendChild(submitButton);
+
+
+  form.appendChild(buttonArea);
+
+  container.appendChild(frag);
+}
+
+export { renderPlayersBoards, renderBoard, gameOver, makeCoordinateForm };
---
+++ src/playerVsComputer.js
@@ -0,0 +1,109 @@
+import './style.css';
+import Player from './Player';
+import {
+  renderPlayersBoards,
+  renderBoard,
+  gameOver,
+  makeCoordinateForm,
+} from './domChanger';
+import computerAttack from './computerAttack';
+
+function playerVsComputer( allCoordinates) {
+  const indexPlayer = new Player();
+
+  const realPlayerBoard = indexPlayer.realPlayer.gameBoard;
+  const computerPlayerBoard = indexPlayer.computerPlayer.gameBoard;
+
+  for(let i = 0; i < allCoordinates.length; i +=1){
+    realPlayerBoard.setShipAt(...allCoordinates[i]);
+  }
+
+  
+  computerPlayerBoard.setShipAt([6, 3], [7, 3], [8, 3], [9, 3]);
+
+  renderPlayersBoards();
+
+  const boards = document.querySelectorAll('.player');
+
+  boards.forEach((board) => {
+    board.addEventListener('click', (e) => {
+      const clicked = e.target;
+      const clickedType = clicked.classList[0];
+      const parent = clicked.parentNode.parentNode.classList[0];
+      if (clickedType === 'coordinate' && parent === 'computerPlayer') {
+        const clickedPosition = clicked.classList[1].slice(1, 3).split('');
+        const hitOrMiss = computerPlayerBoard.receiveAttack(clickedPosition);
+        renderBoard(
+          'computerPlayer',
+          computerPlayerBoard.getGrid(),
+          computerPlayerBoard.getMissedShots(),
+          computerPlayerBoard.getHitShots()
+        );
+        if (computerPlayerBoard.allSunk() === true) {
+          gameOver('realPlayer', 'computerPlayer');
+        }
+
+        if (hitOrMiss === 'Miss') {
+          let continueAttack;
+          do {
+            continueAttack = realPlayerBoard.receiveAttack(
+              computerAttack(realPlayerBoard.availableCoordinates())
+            );
+            renderBoard(
+              'realPlayer',
+              realPlayerBoard.getGrid(),
+              realPlayerBoard.getMissedShots(),
+              realPlayerBoard.getHitShots()
+            );
+
+            if (realPlayerBoard.allSunk() === true) {
+              gameOver('computerPlayer', 'realPlayer');
+              continueAttack = '';
+            }
+          } while (continueAttack === 'Hit');
+        }
+      }
+    });
+  });
+}
+
+function getAllCoordinates(){
+  const classes = [
+    'four',
+    'three',
+    'threeTwo',
+    'two',
+    'twoTwo',
+    'twoThree',
+    'one',
+    'oneTwo',
+    'oneThree',
+    'oneFour',
+  ];
+  const allCoordinates = [];
+  for (let i = 0; i < classes.length; i += 1) {
+    const currInput = document.querySelector(`#${classes[i]}`);
+    const [value] = [currInput.value];
+    const coordinates = value.replaceAll(')', '').replaceAll('(','').split(',');
+    const shipCoordinates = [];
+    for(let j = 0; j < coordinates.length; j += 2){
+      const array = [];
+      array[0] = coordinates[j];      
+      array[1] = coordinates[1+j];     
+      shipCoordinates.push(array); 
+    }
+    allCoordinates.push(shipCoordinates);
+  }
+  return allCoordinates;
+}
+
+export default () => {
+  makeCoordinateForm('player');
+  const form = document.querySelector('.getCoordinateForm');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const allShipCoordinates = getAllCoordinates();
+    playerVsComputer(allShipCoordinates);
+  });
+};
