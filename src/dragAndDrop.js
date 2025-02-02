51ba303356ecba9aecdd214e66347614813b5509
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-02-02 12:10:06 -0700

    add an identifying class for ship groups
    each set of ships of has there own letter to help identify them
    as a group

--- src/dragAndDrop.js
+++ src/dragAndDrop.js
@@ -6,14 +6,18 @@ import './dragAndDrop.css';
 let currDrag = [];
 
 function setDragAndDropShips(player, shipCoordinates) {
+  const shipLetter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   for (let i = 0; i < shipCoordinates.length; i += 1) {
+    const currShipLetter = shipLetter[i];
+
     for (let j = 0; j < shipCoordinates[i].length; j += 1) {
       const shot = shipCoordinates[i][j];
       const box = document.querySelector(`.${player} .k${shot[0]}${shot[1]}`);
       const dragBox = document.createElement('div');
       dragBox.classList.add('drag');
-      dragBox.classList.add( `ship_${shipCoordinates[i].length}`);
-      dragBox.classList.add( `number_${j}`);
+      dragBox.classList.add(`ship_${shipCoordinates[i].length}`);
+      dragBox.classList.add(`number_${j}`);
+      dragBox.classList.add(`letter${currShipLetter}`);
 
       dragBox.draggable = true;
 
@@ -28,7 +32,7 @@ function setDragAndDropShips(player, shipCoordinates) {
 function setUpGridBoxes(player) {
   const targets = document.querySelectorAll(`.${player} .coordinate`);
   for (let i = 0; i < targets.length; i += 1) {
-    targets[i] .innerHTML = '';
+    targets[i].innerHTML = '';
     targets[i].addEventListener('dragover', (e) => {
       e.preventDefault();
     });
@@ -36,7 +40,7 @@ function setUpGridBoxes(player) {
       e.preventDefault();
     });
     targets[i].addEventListener('drop', () => {
-      for(let j = 0; j < currDrag.length; j +=1){
+      for (let j = 0; j < currDrag.length; j += 1) {
         targets[i].prepend(currDrag[j]);
       }
       currDrag = [];
