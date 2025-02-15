9c4ccdb6015a814767b7e25434a71f5d819213fd
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-02-14 18:24:45 -0700

    import Player use it to set ships on both boards

--- src/playerVsPlayer.js
+++ src/playerVsPlayer.js
@@ -1,6 +1,7 @@
 import { twoPlayerSetup, inBetween } from './domChanger';
 import { dragAndDropInterface, getPosition } from './dragAndDrop';
 import './playerVsPlayerStyles.css';
+import Player from './Player';
 
 function getCoordinates() {
   const letters = 'ABCDEFGHIJ';
@@ -18,7 +19,15 @@ function getCoordinates() {
 }
 
 function makeGame(playerOneCoordinates, playerTwoCoordinates){
-  
+  const indexPlayer = new Player();
+
+  const playerOneBoard = indexPlayer.realPlayer.gameBoard;
+  const playerTwoBoard = indexPlayer.computerPlayer.gameBoard;
+
+  for (let i = 0; i < playerOneCoordinates.length; i += 1) {
+    playerOneBoard.setShipAt(...playerOneCoordinates[i]);
+    playerTwoBoard.setShipAt(...playerTwoCoordinates[i]);
+  }
 }
 
 export default () => {
