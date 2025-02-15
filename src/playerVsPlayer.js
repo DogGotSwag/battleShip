3d85cc574812ddc734e471de60748a7d53397c0f
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-02-14 18:11:25 -0700

    add message parameter to inBetween
    will be used so I can reuse function with different message for each
    player

--- src/playerVsPlayer.js
+++ src/playerVsPlayer.js
@@ -1,58 +1,56 @@
-import { twoPlayerSetup } from "./domChanger";
-import { dragAndDropInterface, getPosition } from "./dragAndDrop";
+import { twoPlayerSetup } from './domChanger';
+import { dragAndDropInterface, getPosition } from './dragAndDrop';
 import './playerVsPlayerStyles.css';
 
-function getCoordinates(){
+function getCoordinates() {
   const letters = 'ABCDEFGHIJ';
   const coordinates = [];
-    for (let i = 0; i < letters.length; i += 1) {
-      const shipCoordinates = [];
-      const shipGroup = document.querySelectorAll(`.drag.${letters[i]}`);
-      for (let j = 0; j < shipGroup.length; j += 1) {
-        shipCoordinates.push(getPosition(shipGroup[j].parentNode));
-      }
-      coordinates.push(shipCoordinates);
+  for (let i = 0; i < letters.length; i += 1) {
+    const shipCoordinates = [];
+    const shipGroup = document.querySelectorAll(`.drag.${letters[i]}`);
+    for (let j = 0; j < shipGroup.length; j += 1) {
+      shipCoordinates.push(getPosition(shipGroup[j].parentNode));
     }
+    coordinates.push(shipCoordinates);
+  }
 
   return coordinates;
 }
 
-function addClassToNodeList(nodeList, className){
-  for(let i = 0; i < nodeList.length; i+=1){
+function addClassToNodeList(nodeList, className) {
+  for (let i = 0; i < nodeList.length; i += 1) {
     nodeList[i].classList.add(className);
   }
 }
 
-function removeClassToNodeList(nodeList, className){
-  for(let i = 0; i < nodeList.length; i+=1){
+function removeClassToNodeList(nodeList, className) {
+  for (let i = 0; i < nodeList.length; i += 1) {
     nodeList[i].classList.remove(className);
   }
 }
 
-function inBetween(player){
+function inBetween(player, message) {
   const board = document.querySelector(`.${player}`);
   board.classList.add('whiteWallArea');
-  const boardChildren = document.querySelectorAll(`.${player} > *`);  
+  const boardChildren = document.querySelectorAll(`.${player} > *`);
 
   addClassToNodeList(boardChildren, 'disappear');
 
   const whiteWall = document.createElement('div');
   whiteWall.classList.add('whiteWall');
-  whiteWall.innerText = "Switch to Player 2 click when ready";
+  whiteWall.innerText = message;
 
   board.appendChild(whiteWall);
 
-  whiteWall.addEventListener('click', ()=> {
+  whiteWall.addEventListener('click', () => {
     board.removeChild(whiteWall);
     board.classList.remove('whiteWallArea');
     removeClassToNodeList(boardChildren, 'disappear');
   });
 }
 
-
-
 export default () => {
-  twoPlayerSetup("player");
+  twoPlayerSetup('player');
 
   let playerOneCoordinates = [];
   let playerTwoCoordinates = [];
@@ -70,7 +68,6 @@ export default () => {
       playerTwoCoordinates = getCoordinates();
     });
 
-    inBetween('player');
-
+    inBetween('player', 'Switch to Player 2 click when ready');
   });
 };
